30225e4183b1768b1c05464e1dc32e98a9096f18
alnaimi-github <alnymybdalwas1@gmail.com>
2025-01-12 00:04:31 +0300

    Update Docker setup and enhance Product model
    Updated Dockerfile to use .NET 9.0 images and added comments and stages for building and running the application. Exposed ports 8080 and 8081. Added default values to Product.cs properties. Updated docker-compose files to include catalog.api service. Added Dockerfile.original and Dockerfile1.original with multi-stage build process.

--- src/Services/Catalog/Catalog.API/Modules/Product.cs
+++ src/Services/Catalog/Catalog.API/Modules/Product.cs
@@ -5,10 +5,10 @@ public class Product
    
 
     public Guid Id { get;set; }
-    public string Name { get; set; }
-    public List<string> Category { get; set; }
-    public string Description { get; set; }
-    public string ImageFile { get;  set; }
+    public string Name { get; set; } = string.Empty;
+    public List<string> Category { get; set; } = [];
+    public string Description { get; set; } = string.Empty;
+    public string ImageFile { get; set; } = string.Empty;
     public decimal Price { get;  set; }
 
    
