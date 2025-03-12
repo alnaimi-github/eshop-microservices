c94f8c377a847efdd958e65231802bdd39cc2011
alnaimi-github <alnymybdalwas1@gmail.com>
2025-03-13 00:16:20 +0300

    Add SQLite support and implement DiscountService
    Updated Discount.Grpc.csproj to include Mapster, EF Core SQLite, and tools. Removed discount.proto from <None> and Services folder. Added using directives and configured DiscountContext with SQLite in Program.cs. Modified discount.proto to change Id type to int32. Updated appsettings.json with SQLite connection string. Added DiscountContext.cs defining DbContext and seeding data. Added Extensions.cs with UseMigration method. Added migration files for Coupons table creation and seeding. Implemented DiscountService with GetDiscount method.

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,23 @@
+﻿using Discount.Grpc.Data;
+using Discount.Grpc.Models;
+using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Services;
+
+public class DiscountService(DiscountContext dbContext, ILogger<DiscountService> logger) 
+    : DiscountProtoService.DiscountProtoServiceBase
+{
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await dbContext.Coupons.FirstOrDefaultAsync(x => x.ProductName == request.ProductName) 
+                     ?? new Coupon { ProductName = "No Discount",Amount = 0, Description = "No Discount Description"};
+
+        logger.LogInformation("Discount is retrieved for ProductName : {productName}, Amount : {amount}", 
+            coupon.ProductName,coupon.Amount);
+
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel;
+    }
+}
