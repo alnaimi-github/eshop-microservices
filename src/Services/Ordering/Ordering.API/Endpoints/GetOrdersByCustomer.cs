e864dffd8700c8bd3b884007c13d4c05392a55d2
alnaimi-github <alnymybdalwas1@gmail.com>
2025-04-06 22:47:59 +0300

    Ordering.API Layer Exposing Minimal Api Endpoints with Carter and REPR Pattern!

---
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
@@ -0,0 +1,27 @@
+﻿using Ordering.Application.Orders.Queries.GetOrderByCustomer;
+
+namespace Ordering.API.Endpoints;
+
+public sealed record GetOrdersByCustomerResponse(IEnumerable<OrderDto> Orders);
+public sealed class GetOrdersByCustomer : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/customer/{customerId:guid}", async (Guid customerId, ISender sender) =>
+            {
+                var query = new GetOrdersByCustomerQuery(customerId);
+
+                var result = await sender.Send(query);
+
+                var response = result.Adapt<GetOrdersByCustomerResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetOrdersByCustomer")
+            .Produces<GetOrdersByCustomer>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Get Orders By Customer")
+            .WithDescription("Get Orders By Customer");
+    }
+}
